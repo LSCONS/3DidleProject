c6dc4fdfe2dea53b9e4613ff4a8f73869853324c
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-03-27 20:53:35 +0900

    ✨ Feat: "아이템 데이터 스크립트 추가"

---
+++ Assets/SJW/Item/ItemData.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    UseItem,        //사용 아이템
+    EquipItem       //장비 아이템
+}
+
+public enum EquipItemType
+{
+    Helmet,       //헬멧
+    Armor,        //갑바
+    Shoes,        //신발
+}
+
+public enum UseItemType
+{
+    HP,
+    MP
+}
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class ItemData : ScriptableObject
+{
+    public int ID;
+    public string Name;
+    public string Description;
+    public ItemType Type;
+    public UseItemData[] useItemDatas;
+    public EquipItemData[] equipItemData;
+
+    public bool IsStack;
+    public int MaxStack;
+}
+
+
+[Serializable]
+public class UseItemData
+{
+    public UseItemType UseType;
+    public int HealthValue;
+}
+
+
+[Serializable]
+public class EquipItemData
+{
+    public EquipItemType EquipType;
+    public int ArmorValue;
+}
