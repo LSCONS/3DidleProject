8f9d906f26c7840c4cce340a6992f9452d13fc71
Michael <kimjinhong01@naver.com>
2025-04-01 19:09:25 +0900

    Feat: Fade 화면 구현

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] MenuController pauseMenu;
     [SerializeField] MenuController settingsMenu;
     [SerializeField] Image fade;
+    bool isFade;
     bool isSettings;
 
     private void Update()
@@ -25,7 +27,10 @@ public class UIManager : MonoBehaviour
     {
         if (!isSettings)
         {
-            fade.gameObject.SetActive(true);
+            StopCoroutine(FadeInOut(isFade));
+            isFade = !isFade;
+            fade.gameObject.SetActive(isFade);
+            StartCoroutine(FadeInOut(isFade));
             pauseMenu.MoveToTarget();
         }
     }
@@ -34,5 +39,29 @@ public class UIManager : MonoBehaviour
     {
         isSettings = !isSettings;
         settingsMenu.MoveToTarget();
+    }
+
+    IEnumerator FadeInOut(bool isFade)
+    {
+        if (isFade)
+        {
+            float fadeCount = 0;
+            while (fadeCount < 0.7f)
+            {
+                fadeCount += 0.1f;
+                yield return new WaitForSeconds(0.01f);
+                fade.color = new Color(0, 0, 0, fadeCount);
+            }
+        }
+        else
+        {
+            float fadeCount = 0.7f;
+            while (fadeCount > 0)
+            {
+                fadeCount -= 0.1f;
+                yield return new WaitForSeconds(0.01f);
+                fade.color = new Color(0, 0, 0, fadeCount);
+            }
+        }
     }
 }
