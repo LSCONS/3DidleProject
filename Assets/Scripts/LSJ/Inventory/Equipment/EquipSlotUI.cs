812299e64715651ffae09df327c28f27dfb44b0f
sungmars <136216542+sungmars@users.noreply.github.com>
2025-04-02 21:39:26 +0900

    👷<Chore>: “인벤토리 상점 비쥬얼 업데이트”

--- Assets/Scripts/LSJ/Inventory/Equipment/EquipSlotUI.cs
+++ Assets/Scripts/LSJ/Inventory/Equipment/EquipSlotUI.cs
@@ -40,7 +40,6 @@ public class EquipSlotUI : MonoBehaviour, IDropHandler, IPointerClickHandler
         if (item == null)
         {
             icon.sprite = null;
-            
         }
         else
         {
